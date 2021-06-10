b321de129bbe5639cb53c9b7e10f9ba485b4398b
Maricomber <maricomber@hotmail.com>
2021-06-10 10:05:48 -0300

    primeiro commit

---
+++ src/test/java/com/loja_airsoft/LojaAirsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja_airsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaAirsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
