288c9c6040dd67f643eab8305b1ef6344625f8ef
Maricomber <maricomber@hotmail.com>
2021-10-12 00:04:21 -0300

    Tela de cadastro salvando

--- src/main/java/com/loja_airsoft/app/controllers/CargoController.java
+++ src/main/java/com/loja_airsoft/app/controllers/CargoController.java
@@ -3,7 +3,6 @@ package com.loja_airsoft.app.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.loja_airsoft.app.dtos.CargoDto;
 import com.loja_airsoft.app.response.Response;
@@ -41,62 +39,49 @@ public class CargoController {
 				throw new Exception("Cargo não encontrado");
 			}
 			model.addAttribute("cargos", cargosDto);
+			model.addAttribute("cargoDto",new CargoDto());
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
 		return "cargos";
 		
 	}
-	
-	@GetMapping(path = {"/{id}"})
-	public @ResponseBody ResponseEntity<Response<CargoDto>> findById(@PathVariable Integer id){
-		
-		List<String>erros = new ArrayList<String>();
-		Response<CargoDto>response = new Response<CargoDto>();
-		CargoDto cargoDto;
-		
-		try {
-			
-			if(id == null) {
-				throw new Exception("Campos em branco");
-			}
-			
-			cargoDto= this.cargoService.findById(id);
-			
-			if(cargoDto.equals(null)) {
-				throw new Exception("Cargo não encontrado. ");
-			}
-			response.setData(cargoDto);
-			return ResponseEntity.ok(response);
-		}catch (Exception e) {
-			erros.add(e.getMessage());
-			response.setErrors(erros);
-			return ResponseEntity.badRequest().body(response);
-		}
-		
-	}
-	
-	@PutMapping
-	public @ResponseBody ResponseEntity<Response<CargoDto>> update(@RequestBody CargoDto cargoDto){
-		
-		List<String>erros = new ArrayList<String>();
-		Response<CargoDto>response = new Response<CargoDto>();
-		
-		try {
-			cargoDto = this.cargoService.save(cargoDto);
-			if(cargoDto.equals(null)) {
-				return ResponseEntity.badRequest().body(response);
-			}
-		response.setData(cargoDto);
-		return ResponseEntity.ok(response);
-		}catch (Exception e) {
-			erros.add(e.getMessage());
-			response.setErrors(erros);
-			return ResponseEntity.badRequest().body(response);
-		}
 
+	@PostMapping
+	public String cargosById(@ModelAttribute CargoDto cargoDto, ModelMap model) {
+		List<CargoDto>cargos;
+		try {
+			if(cargoDto.getIdCargo() == null) {
+				cargos = cargoService.findCargos();
+				model.put("cargos", cargos);
+				throw new Exception("Cargo não encontrado");
+			}
+			cargoDto = cargoService.findById(cargoDto.getIdCargo());
+			model.put("cargos", cargoDto);
+			
+		}catch (Exception e) {
+			 
+		}
+		return "cargos";
+		
 	}
 	
+	@PostMapping(path = {"/save"})
+	public String saveCargo(@ModelAttribute CargoDto cargoDto, ModelMap model) {
+		List<CargoDto>cargos;
+		try {
+			cargoDto = cargoService.save(cargoDto);
+			cargos = cargoService.findCargos();
+			
+			model.put("cargos", cargos);
+			model.put("cargoDto",new CargoDto());
+			
+		}catch (Exception e) {
+			 
+		}
+		return "cargos";
+		
+	}
 //	@GetMapping
 //	public @ResponseBody ResponseEntity<Response<List<CargoDto>>> findCargos(HttpServletRequest request) {
 //		
@@ -118,47 +103,47 @@ public class CargoController {
 //		}
 //		
 //	}
-	
-	@PostMapping
-	public @ResponseBody ResponseEntity<Response<CargoDto>> saveCargo(@RequestBody CargoDto cargoDto) {
-		
-		Response<CargoDto> response = new Response<CargoDto>();
-		List<String>erros = new ArrayList<String>();
-		
-		try {
-
-			if(cargoDto == null) {
-				throw new Exception("Campos vazios. ");
-			}
-			cargoDto = this.cargoService.save(cargoDto);
-			response.setData(cargoDto);
-			return ResponseEntity.ok(response);
-			
-		}catch (Exception e) {
-			erros.add(e.getMessage());
-			response.setErrors(erros);
-			return ResponseEntity.badRequest().body(response);
-		}
-		
-	}
-	
-	@DeleteMapping("/{id}")
-	public @ResponseBody ResponseEntity<Response<CargoDto>> deleteCargo(@PathVariable Integer id) {
-		
-		Response<CargoDto> response = new Response<CargoDto>();
-		List<String>erros = new ArrayList<String>();
-		
-		try {
-			if(id == null) {
-				throw new Exception("Campos em branco. ");
-			}
-			this.cargoService.delete(id);
-		}catch (Exception e) {
-			erros.add(e.getMessage());
-			response.setErrors(erros);
-			return ResponseEntity.badRequest().body(response);
-		}
-		return ResponseEntity.ok(response);
-	}
-	
+//	
+//	@PostMapping
+//	public @ResponseBody ResponseEntity<Response<CargoDto>> saveCargo(@RequestBody CargoDto cargoDto) {
+//		
+//		Response<CargoDto> response = new Response<CargoDto>();
+//		List<String>erros = new ArrayList<String>();
+//		
+//		try {
+//
+//			if(cargoDto == null) {
+//				throw new Exception("Campos vazios. ");
+//			}
+//			cargoDto = this.cargoService.save(cargoDto);
+//			response.setData(cargoDto);
+//			return ResponseEntity.ok(response);
+//			
+//		}catch (Exception e) {
+//			erros.add(e.getMessage());
+//			response.setErrors(erros);
+//			return ResponseEntity.badRequest().body(response);
+//		}
+//		
+//	}
+//	
+//	@DeleteMapping("/{id}")
+//	public @ResponseBody ResponseEntity<Response<CargoDto>> deleteCargo(@PathVariable Integer id) {
+//		
+//		Response<CargoDto> response = new Response<CargoDto>();
+//		List<String>erros = new ArrayList<String>();
+//		
+//		try {
+//			if(id == null) {
+//				throw new Exception("Campos em branco. ");
+//			}
+//			this.cargoService.delete(id);
+//		}catch (Exception e) {
+//			erros.add(e.getMessage());
+//			response.setErrors(erros);
+//			return ResponseEntity.badRequest().body(response);
+//		}
+//		return ResponseEntity.ok(response);
+//	}
+//	
 }
